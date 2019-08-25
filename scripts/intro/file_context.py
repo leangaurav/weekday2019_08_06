f9b60d0c4e6ec8717d462f3a9e2f5b77b6d14f7b
SwatiDShetty <swati.shetty008@gmail.com>
2019-08-25 12:58:42 +0530

    Assignments uploaded

---
+++ scripts/intro/file_context.py
@@ -0,0 +1,7 @@
+with open('tables.txt','r') as f:  #context handler
+	for s in f:
+		print(s, end='')
+    
+    
+		
+f.close()
---
+++ scripts/intro/file_read.py
@@ -0,0 +1,7 @@
+f = open('tables.txt', 'r')
+s = f.read()
+print(s)
+print(repr(s))
+
+f.close()
+
